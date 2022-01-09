18b65e9daf563188ddecc8ca56a1dfec46599820
PATHOMRIT <95558561+PATHOMRIT@users.noreply.github.com>
2022-01-09 09:47:18 +0700

    Update game.dart

--- lib/game.dart
+++ lib/game.dart
@@ -1,54 +1,43 @@
-
 import 'dart:math';
-
-// ignore_for_file: avoid_print
 
 class Game {
   static dynamic _maxRandom;
   static final List<int> _round = [];
   int? _answer;
-  int _count = 0;
+  int countNum = 0;
 
   Game({int maxRandom = 100}) {
     var r = Random();
     _setMaxRandom = maxRandom;
     _answer = r.nextInt(maxRandom) + 1;
   }
-
   int doGuess(int num) {
-    _doCount();
-    if (_answer! == num) {
-      return 0;
-    } else if (_answer! < num) {
+    doCount();
+    if (num > _answer!) {
       return 1;
-    } else {
+    } else if (num < _answer!) {
       return -1;
+    } else {
+      return 0;
     }
   }
-
-  int get getMaxRandom {
+  getmaxRandom() {
     return _maxRandom;
   }
-
-  _doCount() {
-    _count++;
+  doCount() {
+    countNum++;
   }
-
+  int getdoCount() {
+    doround(countNum);
+    return countNum;
+  }
   set _setMaxRandom(int info) {
     _maxRandom = info;
   }
-
-  List<int> get getRound {
-    return _round;
-  }
-
-  int get getCount {
-    _doRound(_count);
-    return _count;
-  }
-  _doRound(int info) {
+  doround(int info){
     _round.add(info);
   }
-
-
+  List<int> get getdoround{
+    return _round;
+  }
 }
